4ff6fe25c39d4f0a6c699923bf45cf9a6c452046
RyujiAMANO <ryuji@ryus.co.jp>
2016-04-27 18:12:38 +0900

    不要コード削除

--- Controller/CabinetFilesEditController.php
+++ Controller/CabinetFilesEditController.php
@@ -511,7 +511,11 @@ class CabinetFilesEditController extends CabinetsAppController {
 
 		$this->request->allowMethod('post', 'delete');
 
-		$cabinetFile = $this->CabinetFile->findByKeyAndIsLatest($key, 1);
+		$conditions = [
+			'CabinetFile.key' => $key,
+			'CabinetFile.is_latest' => 1,
+		];
+		$cabinetFile = $this->CabinetFile->find('first', ['conditions' => $conditions]);
 
 		// 権限チェック
 		if ($this->CabinetFile->canDeleteWorkflowContent($cabinetFile) === false) {
@@ -524,112 +528,6 @@ class CabinetFilesEditController extends CabinetsAppController {
 		return $this->redirect(
 			NetCommonsUrl::actionUrl(
 				array('controller' => 'cabinet_files', 'action' => 'index', 'frame_id' => Current::read('Frame.id'), 'block_id' => Current::read('Block.id'))));
-	}
-
-	public function import() {
-		App::uses('CsvFileReader', 'Files.Utility');
-		if ($this->request->is(array('post', 'put'))) {
-			$file = $this->FileUpload->getTemporaryUploadFile('import_csv');
-			debug($file);
-			$reader = new CsvFileReader($file);
-			foreach($reader as $row){
-				debug($row);
-			}
-		}
-	}
-
-	public function regist() {
-		$UploadFile = ClassRegistry::init('Files.UploadFile');
-		$path = '/var/www/app/app/Plugin/Files/Test/Fixture/logo.gif';
-		$path2 = TMP . 'logo.gif';
-		copy($path, $path2);
-		$UploadFile->registByFilePath($path2, 'cabinets', 'content_key..', 'photo');
-	}
-
-	public function attach_file() {
-		$UploadFile = ClassRegistry::init('Files.UploadFile');
-		$path = '/var/www/app/app/Plugin/Files/Test/Fixture/logo.gif';
-		$path2 = TMP . 'logo.gif';
-		copy($path, $path2);
-		$data = $this->CabinetFile->findByIsLatest(true);
-		//$file = new File($path2);
-		$this->CabinetFile->attachFile($data, 'pdf', $path2);
-
-		$savedData = $this->CabinetFile->findById($data['CabinetFile']['id']);
-	}
-
-	public function temporary_download() {
-		App::uses('TemporaryFile', 'Files.Utility');
-		$file = new TemporaryFile();
-		$file->append('test');
-
-		//$this->Download = $this->Components->load('Files.Download');
-		//return $this->Download->downloadFile($file, ['name', 'test.txt']);
-
-		$this->response->file($file->path, ['name' => 'test.txt']);
-		return $this->response;
-	}
-
-
-	/**
-	 * 配列のCSV出力例
-	 *
-	 * @return CakeResponse|null
-	 */
-	public function csv_download2() {
-
-		if ($this->request->is(array('post', 'put'))) {
-			App::uses('CsvFileWriter', 'Files.Utility');
-
-			$data = [
-					['データID', 'タイトル', '本文', '作成日時'],
-					[1, '薪だなつくりました', '薪だなつくるの大変だったよ', '2015-10-01 10:00:00'],
-					[2, '薪ストーブ点火', '寒くなってきたので薪ストーブに火入れましたよ', '2015-12-01 13:00:00'],
-			];
-			$csvWriter = new CsvFileWriter();
-			foreach($data as $line){
-				$csvWriter->add($line);
-			}
-			$csvWriter->close();
-
-			return $csvWriter->download('export.csv');
-		}
-
-	}
-
-	/**
-	 * Modelから取得したデータの指定カラムだけCSV出力する例
-	 *
-	 * @return CakeResponse
-	 */
-	public function csv_download3() {
-		App::uses('CsvFileWriter', 'Files.Utility');
-
-		$header = [
-			'CabinetFile.id' => 'データID',
-			'CabinetFile.title' => 'タイトル',
-			'CabinetFile.body1' => '本文1',
-			'CabinetFile.publish_start' => '公開日時'
-		];
-		$result = $this->CabinetFile->find('all');
-
-		$csvWriter = new CsvFileWriter(['header' => $header]);
-		foreach($result as $data){
-			$csvWriter->addModelData($data);
-		}
-		$csvWriter->close();
-
-		//$zip = new ZipArchive();
-		//$tmpFile = new TemporaryFile();
-		//$zip->open($tmpFile->path, ZipArchive::CREATE | ZipArchive::OVERWRITE);
-		//$zip->addFile($csvWriter->path);
-		//$zip->close();
-
-		// パスワード
-
-
-		//return $csvWriter->download('export.csv');
-		return $csvWriter->zipDownload('test.zip', '日本語ファイル名.csv', 'pass');
 	}
 
 	/**
