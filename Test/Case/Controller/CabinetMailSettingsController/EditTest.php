b5ea986308c3bdf5576f7df27f00a94d401e94eb
RyujiAMANO <ryuji@ryus.co.jp>
2016-06-21 17:18:10 +0900

    Add MailSettingsController Test

---
+++ Test/Case/Controller/CabinetMailSettingsController/EditTest.php
@@ -0,0 +1,92 @@
+<?php
+/**
+ * CabinetMailSettingsController::edit()のテスト
+ *
+ * @author Noriko Arai <arai@nii.ac.jp>
+ * @author Ryuji AMANO <ryuji@ryus.co.jp>
+ * @link http://www.netcommons.org NetCommons Project
+ * @license http://www.netcommons.org/license.txt NetCommons License
+ * @copyright Copyright 2014, NetCommons Project
+ */
+
+App::uses('NetCommonsControllerTestCase', 'NetCommons.TestSuite');
+
+/**
+ * CabinetMailSettingsController::edit()のテスト
+ *
+ * @author Ryuji AMANO <ryuji@ryus.co.jp>
+ * @package NetCommons\Cabinets\Test\Case\Controller\CabinetMailSettingsController
+ */
+class CabinetMailSettingsControllerEditTest extends NetCommonsControllerTestCase {
+
+/**
+ * Fixtures
+ *
+ * @var array
+ */
+	public $fixtures = array(
+		'plugin.cabinets.cabinet',
+		'plugin.cabinets.cabinet_file',
+		'plugin.cabinets.cabinet_file_tree',
+		'plugin.cabinets.cabinet_setting',
+		'plugin.workflow.workflow_comment',
+		'plugin.mails.mail_setting_fixed_phrase',
+	);
+
+/**
+ * Plugin name
+ *
+ * @var string
+ */
+	public $plugin = 'cabinets';
+
+/**
+ * Controller name
+ *
+ * @var string
+ */
+	protected $_controller = 'cabinet_mail_settings';
+
+/**
+ * setUp method
+ *
+ * @return void
+ */
+	public function setUp() {
+		parent::setUp();
+
+		//ログイン
+		TestAuthGeneral::login($this);
+	}
+
+/**
+ * tearDown method
+ *
+ * @return void
+ */
+	public function tearDown() {
+		//ログアウト
+		TestAuthGeneral::logout($this);
+
+		parent::tearDown();
+	}
+
+/**
+ * edit()アクションのGetリクエストテスト
+ *
+ * @return void
+ */
+	public function testEditGet() {
+		//テストデータ
+		$frameId = '6';
+		$blockId = '2';
+
+		//テスト実行
+		$this->_testGetAction(array('action' => 'edit', 'block_id' => $blockId, 'frame_id' => $frameId),
+				array('method' => 'assertNotEmpty'), null, 'view');
+
+		//チェック
+		$this->assertInput('form', null, 'cabinets/cabinet_mail_settings/edit/' . $blockId, $this->view);
+	}
+
+}
