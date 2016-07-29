bb6df3c3410aca81664cc9221152ddb262089da7
RyujiAMANO <ryuji@ryus.co.jp>
2016-07-29 18:21:13 +0900

    テストの雛形を追加

---
+++ Test/Case/Controller/CabinetFilesEditController/BeforeFilterTest.php
@@ -0,0 +1,87 @@
+<?php
+/**
+ * CabinetFilesEditController::beforeFilter()のテスト
+ *
+ * @author Noriko Arai <arai@nii.ac.jp>
+ * @author Ryuji AMANO <ryuji@ryus.co.jp>
+ * @link http://www.netcommons.org NetCommons Project
+ * @license http://www.netcommons.org/license.txt NetCommons License
+ * @copyright Copyright 2014, NetCommons Project
+ */
+
+App::uses('NetCommonsControllerTestCase', 'NetCommons.TestSuite');
+
+/**
+ * CabinetFilesEditController::beforeFilter()のテスト
+ *
+ * @author Ryuji AMANO <ryuji@ryus.co.jp>
+ * @package NetCommons\Cabinets\Test\Case\Controller\CabinetFilesEditController
+ */
+class CabinetFilesEditControllerBeforeFilterTest extends NetCommonsControllerTestCase {
+
+/**
+ * Fixtures
+ *
+ * @var array
+ */
+	public $fixtures = array(
+		'plugin.cabinets.cabinet',
+		'plugin.cabinets.cabinet_file',
+		'plugin.cabinets.cabinet_file_tree',
+		'plugin.cabinets.cabinet_setting',
+		'plugin.workflow.workflow_comment',
+	);
+
+/**
+ * Plugin name
+ *
+ * @var string
+ */
+	public $plugin = 'cabinets';
+
+/**
+ * Controller name
+ *
+ * @var string
+ */
+	protected $_controller = 'cabinet_files_edit';
+
+/**
+ * setUp method
+ *
+ * @return void
+ */
+	public function setUp() {
+		parent::setUp();
+
+		//ログイン
+		TestAuthGeneral::login($this);
+	}
+
+/**
+ * tearDown method
+ *
+ * @return void
+ */
+	public function tearDown() {
+		//ログアウト
+		TestAuthGeneral::logout($this);
+
+		parent::tearDown();
+	}
+
+/**
+ * index()アクションのGetリクエストテスト
+ *
+ * @return void
+ */
+	public function testBeforeFilterGet() {
+		//テスト実行
+		$this->_testGetAction(array('action' => 'index'), array('method' => 'assertNotEmpty'), null, 'view');
+
+		//チェック
+		//TODO:assert追加
+		debug($this->view);
+	}
+
+}
---
+++ Test/Case/Controller/CabinetFilesEditController/SelectFolderTest.php
@@ -0,0 +1,180 @@
+<?php
+/**
+ * CabinetFilesEditController::select_folder()のテスト
+ *
+ * @author Noriko Arai <arai@nii.ac.jp>
+ * @author Ryuji AMANO <ryuji@ryus.co.jp>
+ * @link http://www.netcommons.org NetCommons Project
+ * @license http://www.netcommons.org/license.txt NetCommons License
+ * @copyright Copyright 2014, NetCommons Project
+ */
+
+App::uses('WorkflowControllerIndexTest', 'Workflow.TestSuite');
+
+/**
+ * CabinetFilesEditController::select_folder()のテスト
+ *
+ * @author Ryuji AMANO <ryuji@ryus.co.jp>
+ * @package NetCommons\Cabinets\Test\Case\Controller\CabinetFilesEditController
+ */
+class CabinetFilesEditControllerSelectFolderTest extends WorkflowControllerIndexTest {
+
+/**
+ * Fixtures
+ *
+ * @var array
+ */
+	public $fixtures = array(
+		'plugin.cabinets.cabinet',
+		'plugin.cabinets.cabinet_file',
+		'plugin.cabinets.cabinet_file_tree',
+		'plugin.cabinets.cabinet_setting',
+		'plugin.workflow.workflow_comment',
+	);
+
+/**
+ * Plugin name
+ *
+ * @var string
+ */
+	public $plugin = 'cabinets';
+
+/**
+ * Controller name
+ *
+ * @var string
+ */
+	protected $_controller = 'cabinet_files_edit';
+
+/**
+ * テストDataの取得
+ *
+ * @return array
+ */
+	private function __data() {
+		$frameId = '6';
+		$blockId = '2';
+
+		$data = array(
+			'action' => 'index',
+			'frame_id' => $frameId,
+			'block_id' => $blockId,
+		);
+
+		return $data;
+	}
+
+/**
+ * indexアクションのテスト(ログインなし)用DataProvider
+ *
+ * ### 戻り値
+ *  - urlOptions: URLオプション
+ *  - assert: テストの期待値
+ *  - exception: Exception
+ *  - return: testActionの実行後の結果
+ *
+ * @return array
+ */
+	public function dataProviderIndex() {
+		$data = $this->__data();
+
+		//テストデータ
+		$results = array();
+		$results[0] = array(
+			'urlOptions' => $data,
+			'assert' => array('method' => 'assertNotEmpty'),
+		);
+
+		//TODO:必要なテストデータ追加
+
+		return $results;
+	}
+
+/**
+ * indexアクションのテスト
+ *
+ * @param array $urlOptions URLオプション
+ * @param array $assert テストの期待値
+ * @param string|null $exception Exception
+ * @param string $return testActionの実行後の結果
+ * @dataProvider dataProviderIndex
+ * @return void
+ */
+	public function testIndex($urlOptions, $assert, $exception = null, $return = 'view') {
+		//テスト実行
+		parent::testIndex($urlOptions, $assert, $exception, $return);
+
+		//チェック
+		//TODO:view(ctp)ファイルに対するassert追加
+		debug($this->view);
+	}
+
+/**
+ * indexアクションのテスト(作成権限あり)用DataProvider
+ *
+ * ### 戻り値
+ *  - urlOptions: URLオプション
+ *  - assert: テストの期待値
+ *  - exception: Exception
+ *  - return: testActionの実行後の結果
+ *
+ * @return array
+ */
+	public function dataProviderIndexByCreatable() {
+		return array($this->dataProviderIndex()[0]);
+	}
+
+/**
+ * indexアクションのテスト(作成権限のみ)
+ *
+ * @param array $urlOptions URLオプション
+ * @param array $assert テストの期待値
+ * @param string|null $exception Exception
+ * @param string $return testActionの実行後の結果
+ * @dataProvider dataProviderIndexByCreatable
+ * @return void
+ */
+	public function testIndexByCreatable($urlOptions, $assert, $exception = null, $return = 'view') {
+		//テスト実行
+		parent::testIndexByCreatable($urlOptions, $assert, $exception, $return);
+
+		//チェック
+		//TODO:view(ctp)ファイルに対するassert追加
+		debug($this->view);
+	}
+
+/**
+ * indexアクションのテスト(編集権限あり)用DataProvider
+ *
+ * ### 戻り値
+ *  - urlOptions: URLオプション
+ *  - assert: テストの期待値
+ *  - exception: Exception
+ *  - return: testActionの実行後の結果
+ *
+ * @return array
+ */
+	public function dataProviderIndexByEditable() {
+		return array($this->dataProviderIndex()[0]);
+	}
+
+/**
+ * indexアクションのテスト(編集権限あり)
+ *
+ * @param array $urlOptions URLオプション
+ * @param array $assert テストの期待値
+ * @param string|null $exception Exception
+ * @param string $return testActionの実行後の結果
+ * @dataProvider dataProviderIndexByEditable
+ * @return void
+ */
+	public function testIndexByEditable($urlOptions, $assert, $exception = null, $return = 'view') {
+		//テスト実行
+		parent::testIndexByEditable($urlOptions, $assert, $exception, $return);
+
+		//チェック
+		//TODO:view(ctp)ファイルに対するassert追加
+		debug($this->view);
+	}
+
+}
