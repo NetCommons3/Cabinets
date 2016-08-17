cb6fa8422331c094174cb6327e167e60cd3f6306
RyujiAMANO <ryuji@ryus.co.jp>
2016-08-17 13:17:29 +0900

    use_auth_keyカラムのマイグレーションが漏れていたので追加

---
+++ Config/Migration/1471407387_add_use_auth_key.php
@@ -0,0 +1,50 @@
+<?php
+class AddUseAuthKey extends CakeMigration {
+
+/**
+ * Migration description
+ *
+ * @var string
+ */
+	public $description = 'add_use_auth_key';
+
+/**
+ * Actions to be performed
+ *
+ * @var array $migration
+ */
+	public $migration = array(
+		'up' => array(
+			'create_field' => array(
+				'cabinet_files' => array(
+					'use_auth_key' => array('type' => 'boolean', 'null' => false, 'default' => '0', 'after' => 'is_folder'),
+				),
+			),
+		),
+		'down' => array(
+			'drop_field' => array(
+				'cabinet_files' => array('use_auth_key'),
+			),
+		),
+	);
+
+/**
+ * Before migration callback
+ *
+ * @param string $direction Direction of migration process (up or down)
+ * @return bool Should process continue
+ */
+	public function before($direction) {
+		return true;
+	}
+
+/**
+ * After migration callback
+ *
+ * @param string $direction Direction of migration process (up or down)
+ * @return bool Should process continue
+ */
+	public function after($direction) {
+		return true;
+	}
+}
