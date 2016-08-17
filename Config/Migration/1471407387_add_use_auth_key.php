55e866e1ffa2ddb233ea87d0e2fdf4675de442c0
RyujiAMANO <ryuji@ryus.co.jp>
2016-08-18 06:38:42 +0900

    Add Comment

--- Config/Migration/1471407387_add_use_auth_key.php
+++ Config/Migration/1471407387_add_use_auth_key.php
@@ -1,4 +1,16 @@
 <?php
+/**
+ * AddUseAuthKey
+ *
+ * @author   Ryuji AMANO <ryuji@ryus.co.jp>
+ * @link http://www.netcommons.org NetCommons Project
+ * @license http://www.netcommons.org/license.txt NetCommons License
+ */
+
+
+/**
+ * Class AddUseAuthKey
+ */
 class AddUseAuthKey extends CakeMigration {
 
 /**
