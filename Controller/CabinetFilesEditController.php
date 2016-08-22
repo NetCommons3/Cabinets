a4dad3035ddbc5b54defc00daed5836e5ea273f1
RyujiAMANO <ryuji@ryus.co.jp>
2016-08-22 09:58:10 +0900

    unzip後のリダイレクト先にframe_idがはいってなかったのを修正 Refs https://github.com/NetCommons3/Cabinets/issues/40

--- Controller/CabinetFilesEditController.php
+++ Controller/CabinetFilesEditController.php
@@ -643,6 +643,7 @@ class CabinetFilesEditController extends CabinetsAppController {
 						'action' => 'index',
 						'key' => $parentCabinetFolder['CabinetFile']['key'],
 						'block_id' => Current::read('Block.id'),
+						'frame_id' => Current::read('Frame.id'),
 					]
 				)
 			);
